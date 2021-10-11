e5a6d9fe205bac64366a063684577dacd2d87feb
RapoLore <92298424+RapoLore@users.noreply.github.com>
2021-10-11 10:28:56 +0200

    Add files via upload

---
+++ RESTfulCRUD/src/main/java/org/o7planning/restfulcrud/model/Employee.java
@@ -0,0 +1,49 @@
+package org.o7planning.restfulcrud.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name ="employee")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Employee {
+	
+	private String empNo;
+	private String empName;
+	private String position;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String empNo, String empName, String position) {
+		this.empName = empName;
+		this.empNo = empNo;
+		this.position = position;
+	}
+
+	public String getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+}
